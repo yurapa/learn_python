4498ce0f4faff724d2b95a78a2871b730da4559e
Yura <yura.paray@gmail.com>
2017-06-29 23:17:11 +0300

    codecademy: chapter #6: done

--- app/app_supermarket.py
+++ app/app_supermarket.py
@@ -1,18 +1,22 @@
 prices = {
-    "banana": 4,
-    "apple": 2,
-    "orange": 1.5,
-    "pear": 3
+    "banana" : 4,
+    "apple"  : 2,
+    "orange" : 1.5,
+    "pear"   : 3,
+}
+stock = {
+    "banana" : 6,
+    "apple"  : 0,
+    "orange" : 32,
+    "pear"   : 15,
 }
 
-stock = {
-    "banana": 6,
-    "apple": 0,
-    "orange": 32,
-    "pear": 15 
-}
+total = 0
 
 for key in prices:
     print key
-    print "price: " + str(prices[key])
-    print "stock: " + str(stock[key])
+    print "price: %s" % prices[key]
+    print "stock: %s" % stock[key]
+    total += prices[key] * stock[key]
+
+print total
