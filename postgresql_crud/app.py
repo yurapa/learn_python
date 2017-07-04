4579ac6bb61045e6d9c39b6bf62c62791dce4bdb
Yura <yura.paray@gmail.com>
2017-07-04 10:42:28 +0300

    strted CRUD

---
+++ postgresql_crud/app.py
@@ -0,0 +1,41 @@
+import psycopg2
+
+conn = psycopg2.connect(
+    database = "test_crud",
+    user = "postgres",
+    password = "111111",
+    host = "127.0.0.1",
+    port = "5432"
+)
+print "Opened database successfully"
+
+# cur = conn.cursor()
+# cur.execute('''CREATE TABLE COMPANY
+#       (ID INT PRIMARY KEY     NOT NULL,
+#       NAME           TEXT    NOT NULL,
+#       AGE            INT     NOT NULL,
+#       ADDRESS        CHAR(50),
+#       SALARY         REAL);''')
+# print "Table created successfully"
+
+# conn.commit()
+# conn.close()
+
+
+cur = conn.cursor()
+
+cur.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+    VALUES (1, 'Paul', 32, 'California', 20000.00 )");
+
+cur.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+    VALUES (2, 'Allen', 25, 'Texas', 15000.00 )");
+
+cur.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+      VALUES (3, 'Teddy', 23, 'Norway', 20000.00 )");
+
+cur.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+      VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 )");
+
+conn.commit()
+print "Records created successfully";
+conn.close()
