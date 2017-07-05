041017366d5d81eee4d228b15e2f6f743f813871
Yura <yura.paray@gmail.com>
2017-07-05 09:49:51 +0300

    ended parser for config file

--- postgresql_crud/app.py
+++ postgresql_crud/app.py
@@ -1,13 +1,48 @@
+import ConfigParser
 import psycopg2
 
-conn = psycopg2.connect(
-    database = "test_crud",
-    user = "postgres",
-    password = "111111",
-    host = "127.0.0.1",
-    port = "5432"
-)
-print "Opened database successfully"
+Config = ConfigParser.ConfigParser()
+Config.read('config.ini')
+
+
+def parse_config_file(section):
+    """Parse config.ini file with DB access
+
+    :return:
+    options for connect with DB
+    """
+
+    db_parameters = {}
+
+    if Config.has_section(section):
+        options = Config.options(section)
+        for option in options:
+            db_parameters[option] = Config.get(section, option)
+    return db_parameters
+
+
+def db_connect(**kwargs):
+    """Connect to DB, using parsed data
+
+    :return:
+    Connecting status.
+    """
+
+    db_query = ''
+
+    for key, value in kwargs.iteritems():
+        db_query += "%s='%s' " % (key, value)
+
+    try:
+        conn = psycopg2.connect(db_query)
+        cursor = conn.cursor()
+        print "Connected!\n"
+    except:
+        print "I am unable to connect to the database."
+
+
+kwargs = parse_config_file("db_access")
+db_connect(**kwargs)
 
 # cur = conn.cursor()
 # cur.execute('''CREATE TABLE COMPANY
@@ -22,20 +57,20 @@ print "Opened database successfully"
 # conn.close()
 
 
-cur = conn.cursor()
+# cur = conn.cursor()
 
-cur.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
-    VALUES (1, 'Paul', 32, 'California', 20000.00 )");
+# cur.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+#     VALUES (1, 'Paul', 32, 'California', 20000.00 )");
 
-cur.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
-    VALUES (2, 'Allen', 25, 'Texas', 15000.00 )");
+# cur.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+#     VALUES (2, 'Allen', 25, 'Texas', 15000.00 )");
 
-cur.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
-      VALUES (3, 'Teddy', 23, 'Norway', 20000.00 )");
+# cur.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+#       VALUES (3, 'Teddy', 23, 'Norway', 20000.00 )");
 
-cur.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
-      VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 )");
+# cur.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+#       VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 )");
 
-conn.commit()
-print "Records created successfully";
-conn.close()
+# conn.commit()
+# print "Records created successfully";
+# conn.close()
